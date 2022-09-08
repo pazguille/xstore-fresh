db8c9b4a4652710d6b4cf132cd34a1ceb03d0338
pazguille <guille87paz@gmail.com>
2022-09-08 19:18:54 -0300

    Remove autoload on videos.

--- islands/VideoPlaylist.jsx
+++ islands/VideoPlaylist.jsx
@@ -4,7 +4,7 @@ export default function GameListIsland() {
   const video = videosSignal.value;
   return (
     <div>
-      {video && video.playlist && video.playlist.length > 0 && video.playlist.map((id) => <lite-youtube foo autoload key={id} videoid={id}></lite-youtube>)}
+      {video && video.playlist && video.playlist.length > 0 && video.playlist.map((id) => <lite-youtube key={id} videoid={id}></lite-youtube>)}
     </div>
   );
 }
