40f4ed950f553498d4c05125ba27a4786ffd63fa
pazguille <guille87paz@gmail.com>
2022-09-09 22:03:29 -0300

    Update cache control header

--- routes/_middleware.js
+++ routes/_middleware.js
@@ -3,8 +3,6 @@ export async function handler(req, ctx) {
   const res = await ctx.next();
   if (accept && accept.includes('text/html')) {
     res.headers.set('Cache-Control', 'public, max-age=0, must-revalidate');
-  } else {
-    res.headers.set('Cache-Control', 'public, max-age=31536000, immutable');
   }
   return res;
 };
