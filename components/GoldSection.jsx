c2a5a1e790304dfd5b3976b2da9650c9fd750aba
pazguille <guille87paz@gmail.com>
2023-08-29 20:02:48 -0300

    Update gold section

--- components/GoldSection.jsx
+++ components/GoldSection.jsx
@@ -3,9 +3,8 @@ export default function GoldSection() {
     <section class="gold">
       <h2>Xbox Live Gold</h2>
       <ul>
-        <li><a href="/gold/gold-new" id="gold-new" class="link">Disponibles</a></li>
         <li><a href="/gold/gold-deals" id="gold-deals" class="link">Ofertas</a></li>
-        <li><a href="/gold/gold-free" id="gold-free" class="link">Días gratis</a></li>
+        <li><a href="/gold/gold-new" id="gold-new" class="link">Disponibles</a></li>
       </ul>
     </section>
   );
